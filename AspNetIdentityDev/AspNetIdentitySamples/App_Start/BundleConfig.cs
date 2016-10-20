3ad94aa02f5a4a1bffc6acc39cfb2445fac84f26
Kyle <engineer_03@webdesigns.com.tw>
2016-10-20 16:14:23 +0800

    install identity samples for sample site.

--- AspNetIdentityDev/AspNetIdentitySamples/App_Start/BundleConfig.cs
+++ AspNetIdentityDev/AspNetIdentitySamples/App_Start/BundleConfig.cs
@@ -1,11 +1,10 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
-namespace AspNetIdentitySamples
+namespace IdentitySample
 {
     public class BundleConfig
     {
-        // 如需「搭配」的詳細資訊，請瀏覽 http://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +13,8 @@ namespace AspNetIdentitySamples
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
-            // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
--- AspNetIdentityDev/AspNetIdentitySamples/Global.asax
+++ AspNetIdentityDev/AspNetIdentitySamples/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetIdentitySamples.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdentitySample.MvcApplication" Language="C#" %>
